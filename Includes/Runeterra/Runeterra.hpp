a9dc6fa4dc1a8fcd525b8580f6ff4f50a1c4be6a
Chris Ohk <utilforever@gmail.com>
2021-01-20 23:37:22 +0900

    feat(rust-to-cpp): Create CMake based project

---
+++ Includes/Runeterra/Runeterra.hpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2021 Runeterra Team
+// Chris Ohk, Dongyeon Park, Seungwon Seo
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef RUNETERRA_HPP
+#define RUNETERRA_HPP
+
+#include <Runeterra/Runeterra.hpp>
+#include <Runeterra/Test.hpp>
+
+#endif  // RUNETERRA_HPP
---
+++ Sources/Runeterra/Test.cpp
@@ -0,0 +1,6 @@
+#include <Runeterra/Test.hpp>
+
+int Add(int a, int b)
+{
+    return a + b;
+} 
