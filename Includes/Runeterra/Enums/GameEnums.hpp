2cf7afa64a2052a53299373d55fe921dfff397ba
Chris Ohk <utilforever@gmail.com>
2021-02-07 17:50:20 +0900

    feat(game): Add enum class 'PlayerType'
    - An enumerator for identifying the player

---
+++ Includes/Runeterra/Enums/GameEnums.hpp
@@ -0,0 +1,21 @@
+// Copyright (c) 2021 Runeterra Team
+// Chris Ohk, Dongyeon Park, Seungwon Seo
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef RUNETERRA_GAME_ENUMS_HPP
+#define RUNETERRA_GAME_ENUMS_HPP
+
+namespace Runeterra
+{
+//! \brief An enumerator for identifying the player.
+enum class PlayerType
+{
+    Random,   //!< The random player.
+    Player1,  //!< The first player.
+    Player2,  //!< The second player.
+};
+}  // namespace Runeterra
+
+#endif  // RUNETERRA_GAME_ENUMS_HPP
