78db09f90cb20ffef6d4bd80448670f756bf99ef
Chris Ohk <utilforever@gmail.com>
2021-02-20 09:28:17 +0900

    feat(deckcode): Add constant 'START_DECK_SIZE'
    - The number of cards that can be stored in the deck at game start

--- Includes/Runeterra/Commons/Constants.hpp
+++ Includes/Runeterra/Commons/Constants.hpp
@@ -16,4 +16,7 @@ const int MAX_KNOWN_VERSION = 2;
 //! The maximum number of cards that is allowed in the deck.
 constexpr int MAX_ALLOWED_IN_DECK = 3;
 
+//! The number of cards that can be stored in the deck at game start.
+constexpr int START_DECK_SIZE = 40;
+
 #endif  // RUNETERRA_CARD_CONSTANTS_HPP
