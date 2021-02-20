5efd8715ec06bc609b2d98477e658c26ae78bc61
Chris Ohk <utilforever@gmail.com>
2021-02-20 16:05:41 +0900

    refactor: Separate declaration and definition code

--- Includes/Runeterra/Commons/Utils.hpp
+++ Includes/Runeterra/Commons/Utils.hpp
@@ -17,56 +17,12 @@ namespace Runeterra
 //! Gets next int variable.
 //! \param stream A stream for decoded base32 string.
 //! \return A next int variable.
-inline int GetNextVarInt(std::vector<uint8_t>* stream)
-{
-    if (stream->empty())
-    {
-        return -1;
-    }
-
-    int numRead = 0;
-    int result = 0;
-    int shift = 0;
-
-    while (true)
-    {
-        const uint8_t c = (*stream)[numRead++];
-        result |= (c & 0x07f) << shift;
-        shift += 7;
-
-        if (!(c & 0x80))
-        {
-            break;
-        }
-    }
-
-    stream->erase(stream->begin(), stream->begin() + numRead);
-
-    return result;
-}
+int GetNextVarInt(std::vector<uint8_t>* stream);
 
 //! Converts enum value region to string.
 //! \param region A enum value to convert.
 //! \return A converted region string.
-inline std::string RegionToString(const Region& region)
-{
-    const std::vector<std::pair<std::string, Region>> regionMap{
-        { "DE", Region::Demacia },      { "FR", Region::Freljord },
-        { "IO", Region::Ionia },        { "NX", Region::Noxus },
-        { "PZ", Region::PiltoverZaun }, { "SI", Region::ShadowIsles },
-        { "BW", Region::Bilgewater }
-    };
-
-    for (const auto& elem : regionMap)
-    {
-        if (elem.second == region)
-        {
-            return elem.first;
-        }
-    }
-
-    return "";
-}
+std::string RegionToString(const Region& region);
 }  // namespace Runeterra
 
 #endif  // RUNETERRA_UTILS_HPP
