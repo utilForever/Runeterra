bdd7bb322b40d3a3cb2773747300b3a9a7480d44
Chris Ohk <utilforever@gmail.com>
2021-05-10 00:44:36 +0900

    feat(ecs): Create struct 'Cost'
    - This struct stores a card cost

---
+++ Includes/Runeterra/Components/Cost.hpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2021 Runeterra Team
+// Chris Ohk, Seokmin Hong
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef RUNETERRA_COST_HPP
+#define RUNETERRA_COST_HPP
+
+namespace Runeterra
+{
+//!
+//! \brief Cost struct.
+//!
+//! This struct stores a card cost.
+//!
+struct Cost
+{
+    int cost;
+};
+}  // namespace Runeterra
+
+#endif  // RUNETERRA_COST_HPP
--- Includes/Runeterra/Runeterra.hpp
+++ Includes/Runeterra/Runeterra.hpp
@@ -13,6 +13,7 @@
 #include <Runeterra/Commons/Utils.hpp>
 #include <Runeterra/Components/CardCode.hpp>
 #include <Runeterra/Components/Collectible.hpp>
+#include <Runeterra/Components/Cost.hpp>
 #include <Runeterra/Components/Name.hpp>
 #include <Runeterra/Core/Engine.hpp>
 #include <Runeterra/Decks/Deck.hpp>
