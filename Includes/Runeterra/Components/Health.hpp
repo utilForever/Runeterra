a1adbedd9becdddae5ec4dbbba0ce4e213868b7d
Chris Ohk <utilforever@gmail.com>
2021-05-12 00:56:20 +0900

    feat(ecs): Create struct 'Health'
    - This struct stores the health of the unit

---
+++ Includes/Runeterra/Components/Health.hpp
@@ -0,0 +1,24 @@
+// Copyright (c) 2021 Runeterra Team
+// Chris Ohk, Seokmin Hong
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef RUNETERRA_HEALTH_HPP
+#define RUNETERRA_HEALTH_HPP
+
+namespace Runeterra
+{
+//!
+//! \brief Health struct.
+//!
+//! This struct stores the health of the unit.
+//!
+struct Health
+{
+    int health;
+    int baseHealth;
+};
+}  // namespace Runeterra
+
+#endif  // RUNETERRA_HEALTH_HPP
--- Includes/Runeterra/Runeterra.hpp
+++ Includes/Runeterra/Runeterra.hpp
@@ -15,6 +15,7 @@
 #include <Runeterra/Components/CardCode.hpp>
 #include <Runeterra/Components/Collectible.hpp>
 #include <Runeterra/Components/Cost.hpp>
+#include <Runeterra/Components/Health.hpp>
 #include <Runeterra/Components/Name.hpp>
 #include <Runeterra/Core/Engine.hpp>
 #include <Runeterra/Decks/Deck.hpp>
