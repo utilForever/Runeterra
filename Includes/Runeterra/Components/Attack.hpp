7bc0a0c07006f1e8429638f97a1534128bdd9c3c
Chris Ohk <utilforever@gmail.com>
2021-05-12 00:52:53 +0900

    feat(ecs): Create struct 'Attack'
    - This struct stores the attack of the unit

---
+++ Includes/Runeterra/Components/Attack.hpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2021 Runeterra Team
+// Chris Ohk, Seokmin Hong
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef RUNETERRA_ATTACK_HPP
+#define RUNETERRA_ATTACK_HPP
+
+namespace Runeterra
+{
+//!
+//! \brief Attack struct.
+//!
+//! This struct stores the attack of the unit.
+//!
+struct Attack
+{
+    int attack;
+};
+}  // namespace Runeterra
+
+#endif  // RUNETERRA_ATTACK_HPP
--- Includes/Runeterra/Runeterra.hpp
+++ Includes/Runeterra/Runeterra.hpp
@@ -11,6 +11,7 @@
 #include <Runeterra/Commons/Constants.hpp>
 #include <Runeterra/Commons/Macros.hpp>
 #include <Runeterra/Commons/Utils.hpp>
+#include <Runeterra/Components/Attack.hpp>
 #include <Runeterra/Components/CardCode.hpp>
 #include <Runeterra/Components/Collectible.hpp>
 #include <Runeterra/Components/Cost.hpp>
