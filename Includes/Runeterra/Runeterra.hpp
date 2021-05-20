192e9e2a41c8ba991b708289f974025071064d51
Chris Ohk <utilforever@gmail.com>
2021-05-20 23:20:49 +0900

    refactor: Delete class 'Deck'

--- Includes/Runeterra/Runeterra.hpp
+++ Includes/Runeterra/Runeterra.hpp
@@ -19,7 +19,6 @@
 #include <Runeterra/Components/Health.hpp>
 #include <Runeterra/Components/Name.hpp>
 #include <Runeterra/Core/Engine.hpp>
-#include <Runeterra/Decks/Deck.hpp>
 #include <Runeterra/Enums/CardEnums.hpp>
 #include <Runeterra/Enums/GameEnums.hpp>
 #include <Runeterra/Enums/TaskEnums.hpp>
