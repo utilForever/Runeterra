4e7a80570427f7b6bed24a223dbf46782a7aa500
Chris Ohk <utilforever@gmail.com>
2021-07-04 14:22:35 +0900

    refactor: Delete class 'Engine'

--- Includes/Runeterra/Runeterra.hpp
+++ Includes/Runeterra/Runeterra.hpp
@@ -22,6 +22,5 @@
 #include <Runeterra/Helpers/CardHelpers.hpp>
 #include <Runeterra/Helpers/DeckCodeHelpers.hpp>
 #include <Runeterra/Runeterra.hpp>
-#include <Runeterra/Systems/Engine.hpp>
 
 #endif  // RUNETERRA_HPP
