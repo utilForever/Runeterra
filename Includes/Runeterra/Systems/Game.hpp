90041d0cfb8cd757322e45ca532fdd9747043b40
Chris Ohk <utilforever@gmail.com>
2021-07-04 19:03:49 +0900

    refactor: Add method 'CreatePlayers()'
    - Creates two players to play the game

--- Includes/Runeterra/Systems/Game.hpp
+++ Includes/Runeterra/Systems/Game.hpp
@@ -9,6 +9,9 @@
 
 #include <entt/entt.hpp>
 
+#include <string>
+#include <vector>
+
 namespace Runeterra
 {
 //!
@@ -22,6 +25,12 @@ class Game
  public:
     //! Constructs new game and loads card data.
     Game();
+
+    //! Creates two players to play the game.
+    //! \param deck1 A list of cards to create the deck for player 1.
+    //! \param deck2 A list of cards to create the deck for player 2.
+    void CreatePlayers(const std::vector<std::string>& deck1,
+                       const std::vector<std::string>& deck2);
 
  private:
     entt::registry m_registry;
--- Sources/Runeterra/Systems/Game.cpp
+++ Sources/Runeterra/Systems/Game.cpp
@@ -4,6 +4,9 @@
 // personal capacity and are not conveying any rights to any intellectual
 // property of any third parties.
 
+#include <Runeterra/Commons/Tags.hpp>
+#include <Runeterra/Components/Deck.hpp>
+#include <Runeterra/Components/Name.hpp>
 #include <Runeterra/Helpers/CardHelpers.hpp>
 #include <Runeterra/Systems/Game.hpp>
 
@@ -12,5 +15,19 @@ namespace Runeterra
 Game::Game()
 {
     Card::LoadData(m_registry);
+}
+
+void Game::CreatePlayers(const std::vector<std::string>& deck1,
+                         const std::vector<std::string>& deck2)
+{
+    auto entity1 = m_registry.create();
+    m_registry.emplace<Tag::Player>(entity1);
+    m_registry.emplace<Name>(entity1, "Player 1");
+    m_registry.emplace<Deck>(entity1, deck1);
+
+    auto entity2 = m_registry.create();
+    m_registry.emplace<Tag::Player>(entity2);
+    m_registry.emplace<Name>(entity2, "Player 2");
+    m_registry.emplace<Deck>(entity2, deck1);
 }
 }  // namespace Runeterra
