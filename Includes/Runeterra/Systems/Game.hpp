4f68dc56cbc2f9a9e093e412da1506c6b2ceb4a2
Chris Ohk <utilforever@gmail.com>
2021-07-04 14:30:49 +0900

    refactor: Create class 'Game'
    - This class stores Legends of Runeterra game states which consists of information of both players

---
+++ Includes/Runeterra/Systems/Game.hpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2021 Runeterra Team
+// Chris Ohk, Seokmin Hong
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef RUNETERRA_GAME_HPP
+#define RUNETERRA_GAME_HPP
+
+namespace Runeterra
+{
+//!
+//! \brief Game class.
+//!
+//! This class stores Legends of Runeterra game states which consists of
+//! information of both players.
+//!
+class Game
+{
+};
+}  // namespace Runeterra
+
+#endif  // RUNETERRA_GAME_HPP
---
+++ Sources/Runeterra/Systems/Game.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Runeterra Team
+// Chris Ohk, Seokmin Hong
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#include <Runeterra/Systems/Game.hpp>
+
+namespace Runeterra
+{
+}  // namespace Runeterra
