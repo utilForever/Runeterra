588243f457f4afdfd9873501451ebaeb2af0cd3f
Chris Ohk <utilforever@gmail.com>
2022-01-09 23:31:31 +0900

    feat(game): Add function 'CreatePlayers()'
    - Creates two players to play the game

---
+++ Includes/Runeterra/Systems/GameSystem.hpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2021 Runeterra Team
+// Chris Ohk, Seokmin Hong, Woojin Kim
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef RUNETERRA_GAME_SYSTEM_HPP
+#define RUNETERRA_GAME_SYSTEM_HPP
+
+#include <entt/entt.hpp>
+
+#include <string>
+#include <vector>
+
+namespace Runeterra::Game
+{
+//! Creates two players to play the game.
+//! \param registry A registry that handles entities.
+//! \param deck1 A list of cards to create the deck for player 1.
+//! \param deck2 A list of cards to create the deck for player 2.
+void CreatePlayers(entt::registry& registry,
+                   const std::vector<std::string>& deck1,
+                   const std::vector<std::string>& deck2);
+}  // namespace Runeterra::Game
+
+#endif  // RUNETERRA_GAME_SYSTEM_HPP
