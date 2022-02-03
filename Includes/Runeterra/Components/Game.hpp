093ce334c34e61b0643b66253698d64ab341a3e4
Chris Ohk <utilforever@gmail.com>
2022-02-03 11:12:26 +0900

    feat(game): Create struct 'Game'
    - This struct stores game configs and states

---
+++ Includes/Runeterra/Components/Game.hpp
@@ -0,0 +1,22 @@
+// Copyright (c) 2021 Runeterra Team
+// Chris Ohk, Seokmin Hong, Woojin Kim
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef RUNETERRA_GAME_HPP
+#define RUNETERRA_GAME_HPP
+
+namespace Runeterra
+{
+//!
+//! \brief Game struct.
+//!
+//! This struct stores game configs and states.
+//!
+struct Game
+{
+};
+}  // namespace Runeterra
+
+#endif  // RUNETERRA_GAME_HPP
--- Includes/Runeterra/Runeterra.hpp
+++ Includes/Runeterra/Runeterra.hpp
@@ -21,6 +21,7 @@
 #include <Runeterra/Components/Collectible.hpp>
 #include <Runeterra/Components/Cost.hpp>
 #include <Runeterra/Components/Deck.hpp>
+#include <Runeterra/Components/Game.hpp>
 #include <Runeterra/Components/Health.hpp>
 #include <Runeterra/Components/Name.hpp>
 #include <Runeterra/Components/Nexus.hpp>
